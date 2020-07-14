b1247097fa4d18702b7090a26fbd866eee644c29
snus-kin <tcarroll@snufk.in>
2020-07-14 12:09:24 +0100

    Add config file support, restructure html generation to object

--- src/html_generation.nim
+++ src/html_generation.nim
@@ -1,50 +1,68 @@
-import os, json
+import os, json, parsecfg
 import moustachu
-let basePath = getCurrentDir() & "/env"
-let templatePath = basePath & "/templates"
-let outputPath = basePath & "/output"
-# this could be extended to theme in a config file easily
-let templateSelected = templatePath & "/snufk.in"
-# make sure that we dont have any orphan non-generated files in the output
-removeDir(outputPath)
+type 
+  htmlGenerator* = ref object of RootObj
+    basePath: string
+    templatePath: string
+    outputPath: string
+    templateSelected: string
+    staticFiles: JsonNode
 
-let staticDirectory = templateSelected & "/static"
-# copy to the output dir so we're not using the templated styles
-copyDir(staticDirectory, outputPath & "/static")
-let staticFiles = newJObject()
-for kind, file in walkDir(outputPath & "/static", relative=true):
-  if kind == pcFile:
-    let filename = splitFile(file).name
-    staticFiles[filename] = %* ("static/" & file)
+proc newHtmlGenerator*(config: Config): htmlGenerator =
+  let basePath = config.getSectionValue("", "basePath") & '/'
+  let templatePath = basePath & config.getSectionValue("", "templatePath") & '/'
+  var hg = htmlGenerator(
+    basePath: basePath,
+    templatePath: templatePath,
+    outputPath: basePath & config.getSectionValue("","outputPath"),
+    templateSelected: templatePath & config.getSectionValue("feel", "theme")
+  )
 
+  if hg.outputPath != hg.basePath:
+    removeDir(hg.outputPath)
+  else:
+    # output path must be set in config
+    stderr.writeLine("ERROR: 'outputPath' value not set in config.ini")
+    quit(1)
 
-proc renderArticle(article: JsonNode): string = 
-  ## Procedure to render an article given the json information
-  let tmp = readFile(templateSelected & "/article.moustachu")
-  article["static"] = staticFiles
-  echo pretty(article)
+  let staticDirectory = hg.templateSelected & "/static"
+  # copy to the output dir so we're not using the templated styles
+  copyDir(staticDirectory, hg.outputPath & "/static")
+  let staticFiles = newJObject()
+  for kind, file in walkDir(hg.outputPath & "/static", relative=true):
+    if kind == pcFile:
+      let filename = splitFile(file).name
+      staticFiles[filename] = %* ("static/" & file)
+  hg.staticFiles = staticFiles
+
+  return hg
+
+proc renderArticle(hg: htmlGenerator, article: JsonNode): string =
+  ## procedure to generate an article given the json info
+  let tmp = readFile(hg.templateSelected & "/article.moustachu")
+  article["static"] = hg.staticFiles
   var context = newContext(article)
   let rendered_file = render(tmp, context)
   return rendered_file
 
-proc generateArticleHtml*(article: JsonNode): void =
+proc generateArticleHtml*(hg: htmlGenerator, article: JsonNode): void =
   ## This procedure generates an article html file given a json node
-  let rendered_file = renderArticle(article)
+  let rendered_file = hg.renderArticle(article)
 
   # write to a user-defined slug TODO see what happens on conflict 
   let slug = '/' & article["metadata"]["slug"].getStr
-  writeFile(outputPath & slug, rendered_file)
+  writeFile(hg.outputPath & slug, rendered_file)
 
-proc renderIndex(articles: JsonNode): string =
+proc renderIndex(hg: htmlGenerator, articles: JsonNode): string =
   ## Given a JsonNode, render an index page with a template
-  let tmp = readFile(templateSelected & "/index.moustachu")
+  let tmp = readFile(hg.templateSelected & "/index.moustachu")
   var context = newContext(articles)
   let rendered_file = render(tmp, context)
   return rendered_file
 
-proc generateIndexHtml*(articles: JsonNode): void =
+proc generateIndexHtml*(hg: htmlGenerator, articles: JsonNode): void =
   ## Given a JsonNode, generate an index page html and write it
   ## Later this could paginate, it would just slice the json up
-  articles["static"] = staticFiles
-  let rendered_file = renderIndex(articles)
-  writeFile(outputPath & "/index.html", rendered_file)
+  articles["static"] = hg.staticFiles
+  let rendered_file = hg.renderIndex(articles)
+  writeFile(hg.outputPath & "/index.html", rendered_file)
--- src/mmb.nim
+++ src/mmb.nim
@@ -1,26 +1,30 @@
-import times, os, json
+import times, os, json, parsecfg
 import html_generation, parsing
 
-let basePath = getCurrentDir() & "/env"
 
-proc walkContent(directory: string): JsonNode =
-  let contentPath = basePath & "/content"
+proc walkContent(hg: htmlGenerator, directory: string): JsonNode =
   let articles = newJArray()
   # now we do the walking
-  for kind, file in walkDir(contentPath):
+  for kind, file in walkDir(directory):
     if kind == pcFile:
       let article = parseArticle(file)
       articles.add article
       # generate a html file at each node
-      generateArticleHtml(article)
+      hg.generateArticleHtml(article)
 
   return articles
 
 when isMainModule:
   let timeStart = now()
   var content: JsonNode = newJObject()
-  content["blog_title"] = %* "Blog"
-  content["articles"] = walkContent("content")
-  generateIndexHtml(content)
+  let basePath = getCurrentDir() & "/env"
+  var config = loadConfig(basePath & "/config.ini")
+
+  let hg = newHtmlGenerator(config)
+
+  content["blog_title"] = %* config.getSectionValue("general", "blogName")
+  content["articles"] = hg.walkContent(basePath & '/' & config.getSectionValue("", "contentPath"))
+
+  hg.generateIndexHtml(content)
   let timeEnd = now()
   echo "Built site in: ", (timeEnd - timeStart).inMilliseconds, "ms"
